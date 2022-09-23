c2eb9b9b2eedb24c89a886df436dc82aa733c29b
FredericoGSilva <frederico.dev@outlook.com>
2022-09-23 00:28:15 -0300

    index.css

--- frontend/frontend/src/App.tsx
+++ frontend/frontend/src/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 
 function App() {
+  return (
+    <h1>Olá mundo</h1>
+  )
 }
 
 export default App
