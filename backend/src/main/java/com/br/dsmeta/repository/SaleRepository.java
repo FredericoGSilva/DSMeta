440d7d0120d17bb6bc795ea7926a0543177a8aed
FredericoGSilva <frederico.dev@outlook.com>
2022-09-26 21:28:38 -0300

    API test

--- backend/src/main/java/com/br/dsmeta/repository/SaleRepository.java
+++ backend/src/main/java/com/br/dsmeta/repository/SaleRepository.java
@@ -1,8 +1,8 @@
 package com.br.dsmeta.repository;
 
 import java.time.LocalDate;
+import java.util.List;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,5 +11,5 @@ import com.br.dsmeta.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long>{
 	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
-	Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
+	List<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 }
--- backend/src/main/java/com/br/dsmeta/service/SaleService.java
+++ backend/src/main/java/com/br/dsmeta/service/SaleService.java
@@ -1,5 +1,22 @@
 package com.br.dsmeta.service;
 
-public class SaleService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.br.dsmeta.entities.Sale;
+import com.br.dsmeta.repository.SaleRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	public List<Sale> findSales (String minDate, String maxDate) {
+		return repository.findAll();
+	}
+	
+	
 }
